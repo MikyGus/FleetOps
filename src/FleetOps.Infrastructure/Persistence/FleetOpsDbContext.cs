b63824762842148f77ac7e8049505a36b8178895
Mikael <57296397+MikyGus@users.noreply.github.com>
2026-03-05 08:12:15 +0100

    feat: add initial migration and PostgreSQL overlap contraints (#11)

--- src/FleetOps.Infrastructure/Persistence/FleetOpsDbContext.cs
+++ src/FleetOps.Infrastructure/Persistence/FleetOpsDbContext.cs
@@ -7,4 +7,23 @@ public sealed class FleetOpsDbContext : DbContext
     public FleetOpsDbContext(DbContextOptions<FleetOpsDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Assignment> Assignments => Set<Assignment>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Assignment>(entity =>
+        {
+           entity.ToTable("assignments");
+
+           entity.HasKey(x => x.Id);
+
+           entity.Property(x => x.Id).HasColumnName("id");
+           entity.Property(x => x.DriverId).HasColumnName("driver_id").IsRequired();
+           entity.Property(x => x.VehicleId).HasColumnName("vehicle_id").IsRequired();
+
+           entity.Property(x => x.StartUtc).HasColumnName("start_utc").IsRequired();
+           entity.Property(x => x.EndUtc).HasColumnName("end_utc").IsRequired(); 
+        });
+    }
 }
---
+++ src/FleetOps.Infrastructure/Persistence/Migrations/20260304190943_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetOps.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "assignments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    driver_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    vehicle_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    start_utc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    end_utc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_assignments", x => x.id);
+                });
+
+                migrationBuilder.Sql(@"CREATE EXTENSION IF NOT EXISTS btree_gist;");
+
+                migrationBuilder.Sql(@"
+                ALTER TABLE assignments
+                ADD CONSTRAINT ck_assignments_time CHECK (end_utc > start_utc);
+                ");
+
+                migrationBuilder.Sql(@"
+                ALTER TABLE assignments
+                ADD CONSTRAINT ex_assignments_driver_no_overlap
+                EXCLUDE USING gist (
+                    driver_id WITH =,
+                    tstzrange(start_utc, end_utc, '[)') WITH &&
+                );
+                ");
+
+                migrationBuilder.Sql(@"
+                ALTER TABLE assignments
+                ADD CONSTRAINT ex_assignments_vehicle_no_overlap
+                EXCLUDE USING gist (
+                    vehicle_id WITH =,
+                    tstzrange(start_utc, end_utc, '[)') WITH &&
+                );
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER TABLE assignments DROP CONSTRAINT IF EXISTS ex_assignments_vehicle_no_overlap;"); 
+            migrationBuilder.Sql(@"ALTER TABLE assignments DROP CONSTRAINT IF EXISTS ex_assignments_driver_no_overlap;"); 
+            migrationBuilder.Sql(@"ALTER TABLE assignments DROP CONSTRAINT IF EXISTS ck_assignments_time;");
+
+            migrationBuilder.DropTable(
+                name: "assignments");
+        }
+    }
+}
