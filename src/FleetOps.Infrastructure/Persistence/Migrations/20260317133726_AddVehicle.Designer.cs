2441b99131c614c4c48318e0fa2c898c142bc38e
Mikael <57296397+MikyGus@users.noreply.github.com>
2026-03-17 14:49:31 +0100

    feat: add Vehicle entity and persistence mapping (#29)

--- src/FleetOps.Infrastructure/Persistence/Migrations/20260317133726_AddVehicle.Designer.cs
+++ src/FleetOps.Infrastructure/Persistence/Migrations/20260317133726_AddVehicle.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FleetOps.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FleetOps.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(FleetOpsDbContext))]
-    partial class FleetOpsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260317133726_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
