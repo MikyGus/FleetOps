c308b0e06c8d8e755be1dae10be33aab65467183
MikyGus <mikygus@gmail.com>
2026-03-21 20:13:47 +0100

    feat: add message on error on read of drivers

--- src/FleetOps.Application/Drivers/GetDrivers/GetDriversQueryValidator.cs
+++ src/FleetOps.Application/Drivers/GetDrivers/GetDriversQueryValidator.cs
@@ -8,14 +8,16 @@ public sealed class GetDriversQueryValidator : AbstractValidator<GetDriversQuery
     public GetDriversQueryValidator()
     {
         RuleFor(x => x.Limit)
-            .GreaterThan(0)
-            .LessThanOrEqualTo(500);
+            .InclusiveBetween(1,500)
+            .WithMessage("{PropertyName} must be between {From} and {To}.");
 
         RuleFor(x => x.Offset)
-            .GreaterThanOrEqualTo(0);
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("{PropertyName} must be greater than or equal to {ComparisonValue}.");
 
         RuleFor(x => x.Name)
             .MaximumLength(200)
-            .When(x => !string.IsNullOrWhiteSpace(x.Name));
+            .When(x => !string.IsNullOrWhiteSpace(x.Name))
+            .WithMessage("{PropertyName} is {TotalLength}, but must be at most {MaxLength} characters long.");
     }
 }
